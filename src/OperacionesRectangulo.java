28e8ae66db4f43bda086419e9a8b70dae8790fe6
Ghakan <nohtrim@gmail.com>
2014-09-25 14:11:03 +0200

    Hecho en casa

---
+++ src/OperacionesRectangulo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class OperacionesRectangulo {
+
+	public static void main (String[] args) {
+	
+	  double altura;
+	  double base;
+          double result;
+
+	  Rectangulo rectangulo= new Rectangulo();
+          System.out.print("Estamos trabajando con un rectangulo");
+
+	  Scanner sc = new Scanner(System.in);
+          System.out.print("\ningresa la altura: ");
+          altura = sc.nextDouble();
+
+	  System.out.print("\ningresa la base: ");
+          base = sc.nextDouble();
+	  
+	  circulo.setAltura(altura);
+	  circulo.setBase(base);
+
+	
+	  result =  rectangulo.area();
+	  System.out.println("El area del rectangulo: " + result); 
+			
+	  result =  rectangulo.perimetro();
+	  System.out.println("El perimetro del rectangulo: " + result); 
+	}
+
+}
---
+++ src/Rectangulo.java
@@ -0,0 +1,43 @@
+public class Circulo {
+
+	private double altura = 0;
+
+	private double base = 0;
+
+
+	public double area() {
+
+		double result;
+		
+		result = this.getBase() * this.getAltura();
+		return result;
+	}
+
+	public double perimetro() {
+
+		double result;
+
+		result = 2 * this.getBase() + 2 * this.getAltura();
+
+		return result;
+	}
+
+	public double getAltura() {
+
+		return altura;
+	}	
+	public double getBase() {
+
+		return altura;
+	}	
+
+	public void setAltura(double num) {
+
+		altura = num;
+	} 
+
+	public void setBase(double num) {
+
+		base = num;
+	} 
+}
